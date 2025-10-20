a446c2963f47ad8ec5d959d9c5b6865a1cbd3418
Michael-Meerovich <03michael.meerovich@kamsc.org>
2025-10-20 13:56:44 -0400

    Update ShoppingCart.java

--- src/main/java/ShoppingCart.java
+++ src/main/java/ShoppingCart.java
@@ -1,10 +1,11 @@
-import java.text.NumberFormat;
+import java.text.DecimalFormat;
 import java.util.Scanner;
 
 public class ShoppingCart {
     public static void main(String[] args) {
         // TODO: Ask the user how many items they are purchasing
         Scanner sc = new Scanner(System.in);
+        DecimalFormat df = new DecimalFormat("0.00");
         int numruns;
         System.out.print ("How many items are you purchasing? ");
         numruns = sc.nextInt();
@@ -15,7 +16,7 @@ public class ShoppingCart {
             System.out.print ("Price of item " + k + "? ");
             price += sc.nextDouble();
         }
-        System.out.print("Your cart has " + numruns + " items with a total cost of $" + price);
+        System.out.print("Your cart has " + numruns + " items with a total cost of $" + df.format(price));
         // TODO: Accumulate the total cost
         // TODO: Print the final summary line with item count and total
         // TODO: Use Scanner for input
